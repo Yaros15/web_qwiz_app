d98c6cd1e79c1ae3c084f9435ff5c4445bd04527
AlenaSlasti <slasti_08@mail.ru>
2026-01-17 00:02:12 +0400

    Initial commit

---
+++ src/main/java/com/example/web_qwiz_app/WebQwizAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_qwiz_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQwizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQwizAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/web_qwiz_app/model/forDel.java
@@ -0,0 +1,4 @@
+package com.example.web_qwiz_app.model;
+
+public class forDel {
+}
